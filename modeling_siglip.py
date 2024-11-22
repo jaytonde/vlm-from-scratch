e78918c0ab0fbe7601a30af1b59fb4c2f1e85655
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-11-22 09:50:53 +0530

    added MLP

--- modeling_siglip.py
+++ modeling_siglip.py
@@ -73,7 +73,21 @@ class SiglipVisionEmbeddings(nn.Module):
         # [Batch_Size, Num_Patches, Embed_Dim]
         return embeddings
 
-
+class SiglipMLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.fc1 = nn.Linear(config.hidden_size, config.intermediate_size)
+        self.fc2 = nn.Linear(config.intermediate_size, config.hidden_size)
+    
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        # [Batch_Size, Num_Patches, Embed_Dim] -> [Batch_Size, Num_Patches, intermediate_size] 
+        hidden_states = self.fc1(hidden_states)
+        # hidden_states : [Batch_Size, Num_Patches, intermediate_size]
+        hidden_states = nn.functional.gelu(hidden_states, approximate="tanh")
+        # [Batch_Size, Num_Patches, intermediate_size] -> [Batch_Size, Num_Patches, Embed_Dim]
+        hidden_states = self.fc2(hidden_states)
+        return hidden_states
 
 class SiglipEncoderLayer(nn.Module):
     def __init__(self, config:SiglipVisionConfig):
