6a5e985cd0b1a97913d65e973a740c1464c51214
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-11-24 12:36:35 +0530

    rtytyuryt

--- processing_paligemma.py
+++ processing_paligemma.py
@@ -6,6 +6,31 @@ import torch
 IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5]
 IMAGENET_STANDARD_STD  = [0.5, 0.5, 0.5]
 
+def resize(
+    image: Image, 
+    size: Tuple[int, int]
+    resample: Image.Resampling = None,
+    reducing_gap : Optional[int] = None, 
+    ) -> np.ndarray:
+
+    height, width = size
+    resized_image = image.resize((width, height), resample=resample, reducing_gap=reducing_gap)
+    return resized_image
+
+def rescale(image: np.ndarray, scale: float, dtype: np.dtype = np.float32) -> np.ndarray:
+    rescaled_image = image * scale
+    rescaled_image = rescaled_image.astype(dtype)
+    return rescaled_image
+
+def normalize(
+    image: np.ndarray,
+    mean: Union[float, Iterable[float]],
+    std: Union[float, Iterable[float]],
+    ) -> np.ndarray:
+    mean  = np.array(mean, dtype=image.dtype)
+    std   = np.array(std, dtype=image.dtype)
+    image = (image - mean) / std
+    return image 
 
 def process_images(
     images         : List[Image.Image],
