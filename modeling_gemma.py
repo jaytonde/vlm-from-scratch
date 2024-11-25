083ac821c3bcde7c9713b1e1e3ae807f7c62b5a7
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-11-25 15:37:53 +0530

    updated the forward method

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -95,5 +95,28 @@ class PaliGemmaForConditionalGeneration(nn.Module):
         attention_mask     : Optional[torch.Tensor] = None,
         kv_cache           : Optional[KVCache]      = None 
     ) -> Tuple:
-        pass
+        # Make sure the input is right-padded
+        assert torch.all(attention_mask == 1), "The input cannot be padded"
+
+        # 1. Extra the input embeddings
+        # shape: (Batch_Size, Seq_Len, Hidden_Size)
+        inputs_embeds = self.language_model.get_input_embeddings()(input_ids)
+
+        # 2. Merge text and images
+        # [Batch_Size, Channels, Height, Width] -> [Batch_Size, Num_Patches, Embed_Dim]
+        selected_image_feature = self.vision_tower(pixel_values.to(inputs_embeds.dtype))
+        # [Batch_Size, Num_Patches, Embed_Dim] -> [Batch_Size, Num_Patches, Hidden_Size]
+        image_features = self.multi_modal_projector(selected_image_feature)
+
+        # Merge the embeddings of the text tokens and the image tokens
+        inputs_embeds, attention_mask, position_ids = self._merge_input_ids_with_image_features(image_features, inputs_embeds, input_ids, attention_mask, kv_cache)
+        
+        outputs = self.language_model(
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+            kv_cache=kv_cache,
+        )
+
+        return outputs
         
