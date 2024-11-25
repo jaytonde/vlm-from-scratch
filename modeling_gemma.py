02461920cb26c346a8c053e7fd1a665736e21ff8
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-11-25 19:03:08 +0530

    added GemmaForCausalLM

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -72,6 +72,52 @@ class PaliGemmaConfig():
         self.text_config.num_image_tokens = (self.vision_config.image_size // self.vision_config.patch_size) ** 2
         self.vision_config.projection_dim = projection_dim
 
+class GemmaForCausalLM(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config     = config
+        self.model      = GemmaModel(config)
+        self.vocab_size = config.vocab_size
+        self.lm_head    = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+
+    def get_input_embeddings(self):
+        return self.model.embed_tokens
+    
+    def tie_weights(self):
+        self.lm_head.weight = self.model.embed_tokens.weight
+
+    def forward(
+        self,
+        attention_mask : Optional[torch.Tensor] = None,
+        position_ids   : Optional[torch.LongTensor] = None,
+        inputs_embeds  : Optional[torch.FloatTensor] = None,
+        kv_cache       : Optional[KVCache] = None,
+        ) -> Tuple:
+
+        # input_embeds: [Batch_Size, Seq_Len, Hidden_Size]
+        # outputs: [Batch_Size, Seq_Len, Hidden_Size]
+        outputs = self.model(
+            attention_mask = attention_mask,
+            position_ids   = position_ids,
+            inputs_embeds  = inputs_embeds,
+            kv_cache       = kv_cache,
+        )
+
+        hidden_states = outputs
+        logits        = self.lm_head(hidden_states)
+        logits        = logits.float()
+
+        return_data = {
+            "logits": logits,
+        }
+
+        if kv_cache is not None:
+            # Return the updated cache
+            return_data["kv_cache"] = kv_cache
+
+        return return_data
+
 class PaliGemmaMultiModelProjector(nn.Module):
     def __init__(self, config: PaliGemmaConfig):
         super().__init__()
