40a29102c6e1f7aa4ebb629c0df47d942a756689
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2024-11-26 09:43:47 +0530

    updated the utils.py

--- utils.py
+++ utils.py
@@ -1,55 +1,38 @@
+from modeling_gemma import PaliGemmaForConditionalGeneration, PaliGemmaConfig
+from transformers import AutoTokenizer
+import json
+import glob
+from safetensors import safe_open
+from typing import Tuple
+import os
 
-from PIL import Image
-import torch
-import fire
+def load_hf_model(model_path: str, device: str) -> Tuple[PaliGemmaForConditionalGeneration, AutoTokenizer]:
+    # Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="right")
+    assert tokenizer.padding_side == "right"
 
-from processing_paligemma import PaliGemmaProcessor
-from modeling_gemma import KVCache, PaliGemmaForConditionalGeneration
-from utils import load_hf_model
+    # Find all the *.safetensors files
+    safetensors_files = glob.glob(os.path.join(model_path, "*.safetensors"))
 
+    # ... and load them one by one in the tensors dictionary
+    tensors = {}
+    for safetensors_file in safetensors_files:
+        with safe_open(safetensors_file, framework="pt", device="cpu") as f:
+            for key in f.keys():
+                tensors[key] = f.get_tensor(key)
 
-def main(
-    model_path: str = None,
-    prompt: str = None,
-    image_file_path: str = None,
-    max_tokens_to_generate: int = 100,
-    temperature: float = 0.8,
-    top_p: float = 0.9,
-    do_sample: bool = False,
-    only_cpu: bool = False,
-):
-    device = "cpu"
+    # Load the model's config
+    with open(os.path.join(model_path, "config.json"), "r") as f:
+        model_config_file = json.load(f)
+        config = PaliGemmaConfig(**model_config_file)
 
-    if not only_cpu:
-        if torch.cuda.is_available():
-            device = "cuda"
-        elif torch.backends.mps.is_available():
-            device = "mps"
+    # Create the model using the configuration
+    model = PaliGemmaForConditionalGeneration(config).to(device)
 
-    print("Device in use: ", device)
+    # Load the state dict of the model
+    model.load_state_dict(tensors, strict=False)
 
-    print(f"Loading model")
-    model, tokenizer = load_hf_model(model_path, device)
-    model = model.to(device).eval()
+    # Tie weights
+    model.tie_weights()
 
-    num_image_tokens = model.config.vision_config.num_image_tokens
-    image_size = model.config.vision_config.image_size
-    processor = PaliGemmaProcessor(tokenizer, num_image_tokens, image_size)
-
-    print("Running inference")
-    with torch.no_grad():
-        test_inference(
-            model,
-            processor,
-            device,
-            prompt,
-            image_file_path,
-            max_tokens_to_generate,
-            temperature,
-            top_p,
-            do_sample,
-        )
-
-
-if __name__ == "__main__":
-    fire.Fire(main)
+    return (model, tokenizer)
